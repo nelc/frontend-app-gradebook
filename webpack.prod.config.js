f27a9535e99167c1cf7e9509040632d4e756538c
Johan Seto Kaiba <51926076+johanseto@users.noreply.github.com>
2024-05-09 14:18:48 -0500

    feat: deploy mfe s3 action (#3)
    * fix: env vars reading for built
    
    cherry-pick of other mfe.
    this is like a cherrypick of this pr in learning.
    https://github.com/nelc/frontend-app-learning/pull/27/files
    This is an error generated of the load of paragonTheme plugin.
    
    * feat: add deploy mfe-s3-action
    
    * fix!: remove mfe_config to use the  action env
    
    * Revert "fix!: remove mfe_config to use the  action env"
    
    This reverts commit 55d22d2dfb7adc46ad0f4fc4b27a1c51ec00719a.
    
    * fix: dont override original env var of the process
    
    With this change I store in a different var in memory the original environment vars.
    This with the purpose of keep them and not override.
    Dotenv dont override them and also with .env analysis this ones would not be override too.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
 const path = require('path');
+const originalEnvConfig = { ...process.env }; //Store original process env vars
 const { createConfig } = require('@edx/frontend-build');
 
 const config = createConfig('webpack-prod');
@@ -9,5 +12,17 @@ config.resolve.modules = [
 ];
 
 config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
+
+// The configuration is generated using the createConfig method from the frontend-build library,
+// this method preloads multiple files to generate the resulting configuration, including webpack.common.config.js,
+// https://github.com/nelc/frontend-build/blob/open-release/palm.nelp/config/webpack.common.config.js,
+// which includes ParagonWebpackPlugin. This plugin, in turn, retrieves its configuration from the .env.development file
+// https://github.com/nelc/frontend-build/blob/open-release/palm.nelp/lib/plugins/paragon-webpack-plugin/ParagonWebpackPlugin.js#L20-L22
+// Therefore, regardless of the configuration type, the plugin always utilizes data from .env.development.
+// The following code overrides this behavior in order to use the .env file.
+const envConfig = dotenv.parse(fs.readFileSync('.env'));
+Object.keys(envConfig).forEach(k => {
+  process.env[k] = originalEnvConfig[k] ?? envConfig[k];
+});
 
 module.exports = config;
